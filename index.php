4afdade2d169c0fe9cb4e2203ebd4a306dc8af23
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-10 23:25:01 -0700

    added image file

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 		<title>Data-Design</title>
 	</head>
 	<body>
-
+		<h1>Entities, Relationships and Attributes</h1>
+		<h2>
+		Subject Website:
+		<a href="https://medium.com/@jameshorrox/the-science-of-hiking-53e8260ab46d" title="The Science of Hiking" target="_blank"
+		>Medium.com</a>
+		</h2>
+		<img src="data-design" alt="elements, attributes and relation diagram"/>
 	</body>
 </html>
