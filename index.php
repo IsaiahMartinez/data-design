4c52f706883edb3d84e0be69a2a83532f329de64
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-11 16:37:32 -0700

    removed diagram and updated list items

--- index.php
+++ index.php
@@ -5,12 +5,76 @@
 		<title>Data-Design</title>
 	</head>
 	<body>
-		<h1>Entities, Relationships and Attributes</h1>
-		<h2>
-		Subject Website:
-		<a href="https://medium.com/@jameshorrox/the-science-of-hiking-53e8260ab46d" title="The Science of Hiking" target="_blank"
-		>Medium.com</a>
-		</h2>
-		<img src="data-design" alt="elements, attributes and relation diagram"/>
+		<h1>Data Design Medium</h1>
+		<h2>Persona:</h2>
+		<ul>
+			<li>
+				<strong>Name:</strong>James Horrox
+			</li>
+			<li>
+				<strong>Gender:</strong>Male
+			</li>
+			<li>
+				<strong>Age:</strong>56
+			</li>
+			<li>
+				<strong>Technology:</strong> Inspiron 15 7000 with Dell wireless keyboard and mouse, KM636.
+			</li>
+				<ul>
+					<li>
+						<strong>Device:</strong> Dell Inspiron 15 7000 with Dell wireless keyboard and mouse. Windows 10.
+					</li>
+					<li>
+						<strong>Proficiency:</strong> Advanced; Helps government and private businesses prepare books and journals 								  articles manuscripts for publication.
+					</li>
+					<li>
+						<strong>Does he love or hate it?</strong> Mr. Horrox loves Microsoft products and uses them exclusively.
+					</li>
+				</ul>
+			<li>
+				<strong>Attitudes and Needs:</strong>
+			</li>
+				<ul>
+					<li>
+						Mr. Horrox is currently a freelance editor. He has a Ph.D and previously taught in various Universities. As 						an editor, Horrox has worked in a variety of industries in 40+ countries. Mr. Horrox is also a researcher in 						the field of publications in humanities and social sciences. He needs a site where he can post his 						writings.
+					</li>
+					<li>
+						Mr. Horrox prefers sites that provide a seamless UX experience. This way he can concentrate on writing and 						posting his works rather than having to think about <strong>how</strong> to post them.
+					</li>
+				</ul>
+		</ul>
+		<h2>User Stories</h2>
+		<p>As a registered user, I want to post my writing online and obtain feedback.</p>
+		<h2>Use Cases:</h2>
+		<p>Mr. Horrox has just signed up for Medium.com and wants to start writing and posting his works.</p>
+		<p><em>precondition:</em> Mr. Horrox needs to sign into his account.</p>
+		<p><em>postcondition:</em>Mr. Horrox successfully posts his works on the website.</p>
+		<h2>Interaction Flow:</h2>
+		<ol>
+			<li>
+				User enters his username and password.
+			</li>
+			<li>
+				The site verifies credentials and allows access.
+			</li>
+			<li>
+				User begins writing and select post.
+			</li>
+			<li>
+				System asks user to confirm they would like to post.
+			</li>
+			<li>
+				User clicks yes to confirm the action.
+			</li>
+			<li>
+				The site posts the users work.
+			</li>
+			<li>
+				User clicks logout to exit the system.
+			</li>
+			<li>
+				System logs user out of site.
+			</li>
+		</ol>
 	</body>
 </html>
